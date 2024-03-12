d9a5c799d903522200e6e69cea7e3878c11b9125
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2024-03-12 22:29:42 +0700

    menambahkan implementasi dengan table driven

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-System.Console.WriteLine("Hello World")
+using static KodePos;
+    
+public class KodePos
+{
+    public enum EnumKodepos
+    {
+        Batununggal, Kujangsari,
+        Mengger, Wates,
+        Cijaura, Jatisari,
+        Sekejati, Kebonwaru,
+        Maleer, Samoja
+    }
+
+    public static String getKodePos(EnumKodepos Wilayah)
+    {
+        String[] outputKode = { "40266", "40287", "40267", "40256", "40287", "40286", "40286", "40286", "40272", "40274", "40273" };
+        return outputKode[(int)Wilayah];
+    }
+}
+
+
+
+public class Hello
+{
+    public static void Main(String[] args)
+    {
+        EnumKodepos list = EnumKodepos.Sekejati;
+        String kodePos = getKodePos(list);
+        Console.WriteLine("wilayah {1}, Kode Pos nya {0}", kodePos, list);
+
+
+    }
+}
